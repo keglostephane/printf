746485372e322811842cc61bdf3cf277b431955a
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-20 22:25:35 +0000

    Add print_str function to chars.c

--- chars.c
+++ chars.c
@@ -10,3 +10,26 @@ int print_char(va_list args)
 	_putchar(va_arg(args, int));
 	return (1);
 }
+
+/**
+ * print_str - prints a string
+ *
+ * @args: list of variable arguments
+ *
+ * Return: the number of characters printed
+ */
+int print_str(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (!str)
+	{
+		str = "(null)";
+		write(1, str, _strlen(str));
+		return (_strlen(str));
+	}
+	else
+		write(1, str, _strlen(str));
+
+	return (_strlen(str));
+}
