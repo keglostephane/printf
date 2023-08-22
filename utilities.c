d23ed32581837c5026631fd97225c876e5c4098f
Chesterkxng <cgoita00@gmail.com>
2023-08-22 10:05:57 -0700

    adding the _memcpy function to utilities.c

--- utilities.c
+++ utilities.c
@@ -28,3 +28,28 @@ int _strlen(char *s)
 		count++;
 	return (count);
 }
+/**
+ * *_memcpy - Entry point
+ * Description: function copies n bytes from memory area
+ * src to memory area dest
+ * @dest : a pointer to a string(dest)
+ * @src :a pointerto a string(src)
+ * @n : integer
+ *
+ * Return: a pointer to a string.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *address = dest;
+
+	i = 0;
+	while (i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	return (address);
+}
