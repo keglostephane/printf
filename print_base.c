4988e4a4f6adb1f96ed2a3cb567b7e7b8edb3657
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-24 20:13:15 +0000

    Add more functions

--- print_base.c
+++ print_base.c
@@ -2,46 +2,6 @@
 #include <stdlib.h>
 
 /**
- * store_base_reverse - store a number converted in a base in reverse
- *
- * @n: number to convert in base
- *
- * @base: the base to convert to
- *
- * @upper: flag for printing in upper case for base greater than 10
- *
- * @buffer: buffer to store result before printing
- *
- * Return: the buffer that contain the number converted in base
- * stored in reverse
- *
- */
-
-char *store_base_reverse(unsigned int n, int base, int upper,
-			 char *buffer)
-{
-	int i, r;
-
-	i = 0;
-	while (n > 0)
-	{
-		r = n % base;
-		if (r < 10)
-			buffer[i++] = r + '0';
-		else
-		{
-			if (upper)
-				buffer[i++] = r + 55;
-			else
-				buffer[i++] = r + 87;
-		}
-		n /= base;
-	}
-
-	return (buffer);
-}
-
-/**
  * print_bin - a function that convert a int to bin
  *
  * @args : argument
@@ -61,6 +21,111 @@ int print_bin(va_list args, char *buffer)
 		return (_putchar(dec + '0'));
 
 	rev_ptr = store_base_reverse(dec, 2, 0, buffer);
+	ptr = rev_str(rev_ptr);
+
+	return (flush_buffer(ptr));
+}
+
+
+/**
+ * print_octal - print an integer in octal base
+ *
+ * @args: list of variable arguments
+ *
+ * @buffer: store the converted number in octal base
+ *
+ * Return: number of digits of the converted integer
+ */
+int print_octal(va_list args, char *buffer)
+{
+	char *ptr, *rev_ptr;
+	unsigned long dec;
+
+	dec = va_arg(args, unsigned long);
+
+	if (dec < 8)
+		return (_putchar(dec + '0'));
+
+	rev_ptr = store_base_reverse(dec, 8, 0, buffer);
+	ptr = rev_str(rev_ptr);
+
+	return (flush_buffer(ptr));
+}
+
+/**
+ * print_unsigned - print an unsigned integer
+ *
+ * @args: list of variable arguments
+ *
+ * @buffer: store the unsigned number
+ *
+ * Return: number of digits of the unsigned integer
+ */
+int print_unsigned(va_list args, char *buffer)
+{
+	char *ptr, *rev_ptr;
+	unsigned long dec;
+
+	dec = va_arg(args, unsigned long);
+
+	if (dec < 10)
+		return (_putchar(dec + '0'));
+
+	rev_ptr = store_base_reverse(dec, 10, 0, buffer);
+	ptr = rev_str(rev_ptr);
+
+	return (flush_buffer(ptr));
+}
+
+/**
+ * print_hexa - print an integer in hexadecimal base
+ *
+ * @args: list of variable arguments
+ *
+ * @buffer: store the converted number in hexadecimal base
+ *
+ * Return: number of digits of the converted integer
+ */
+int print_hexa(va_list args, char *buffer)
+{
+	char *ptr, *rev_ptr;
+	unsigned long dec;
+
+	dec = va_arg(args, unsigned long);
+
+	if (dec < 10)
+		return (_putchar(dec + '0'));
+	if (dec > 9 && dec < 16)
+		return (_putchar(dec + 87));
+
+	rev_ptr = store_base_reverse(dec, 16, 0, buffer);
+	ptr = rev_str(rev_ptr);
+
+	return (flush_buffer(ptr));
+}
+
+/**
+ * print_hexa_up - print an integer in hexadecimal base in uppercase
+ *
+ * @args: list of variable arguments
+ *
+ * @buffer: store the converted number in hexadecimal base
+ *
+ * Return: number of digits of the converted integer
+ */
+int print_hexa_up(va_list args, char *buffer)
+{
+	char *ptr, *rev_ptr;
+	unsigned long dec;
+
+	dec = va_arg(args, unsigned long);
+
+	if (dec < 10)
+		return (_putchar(dec + '0'));
+	if (dec > 9 && dec < 16)
+		return (_putchar(dec + 87));
+
+	rev_ptr = store_base_reverse(dec, 16, 1, buffer);
 	ptr = rev_str(rev_ptr);
 
 	return (flush_buffer(ptr));
